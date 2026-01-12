223f7eae641ff9a433572e6c9d46f0b238837d0a
Hellen Tiemi <htc.albuquerque@gmail.com>
2026-01-12 19:39:40 -0300

    trocada frase e posicao do arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <div>Hello World</div>;
+  return <div>Gabriel, obrigada por tudo, por me ajudar, incentivar e me dar confiança para fazer isso, me apoiar e me motivar a seguir em frente. Te amo muito!</div>;
 }
 
 export default Home;

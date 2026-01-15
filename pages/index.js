60f252f064cacadbfe298c9777e0868286a3daf9
Hellen Tiemi <htc.albuquerque@gmail.com>
2026-01-15 20:06:09 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <div>Gabriel, obrigada por tudo, por me ajudar, incentivar e me dar confiança para fazer isso, me apoiar e me motivar a seguir em frente. Te amo muito!</div>;
+  return (
+    <div>
+      Gabriel, obrigada por tudo, por me ajudar, incentivar e me dar confiança
+      para fazer isso, me apoiar e me motivar a seguir em frente. Te amo muito!
+    </div>
+  );
 }
 
 export default Home;
